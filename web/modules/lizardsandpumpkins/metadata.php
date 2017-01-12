a9476d5e95ffd21dfdb95ef5649a5cdf0892c837
a.ziethen <az@scale.sc>
2017-01-12 12:39:48 +0100

    move controller to modules directory

---
+++ web/modules/lizardsandpumpkins/metadata.php
@@ -0,0 +1,19 @@
+<?php
+
+$aModule = array(
+    'id'            => 'lizzardandpumpkins',
+    'title'         => 'OXID2LIZARD Importer',
+    'description'   => 'Imports product data from Oxid to Lizards and Pumpkins',
+    'thumbnail'     => 'picture.gif',
+    'version'       => '0.1',
+    'author'        => 'ScaleCommerce GmbH / Lizards & Pumpkins UG',
+    'email'         => 'support@scale.sc',
+    'url'           => '',
+    'extend'        => array(),
+    'files'         => array(
+        'azlizardsimport'                => 'lizardsandpumpkins/application/controllers/azlizardsimport.php',
+    ),
+    'templates' => array(
+    ),
+);
+
